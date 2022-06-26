445936863ecfd7554db5934c152c55952924ee60
Nirwash <nirwash@bk.ru>
2022-06-26 18:19:18 +0300

    инициализация поиска

--- app/src/main/java/com/nirwashh/android/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/nirwashh/android/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,14 @@
 package com.nirwashh.android.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import com.nirwashh.android.podplay.R
 import com.nirwashh.android.podplay.databinding.ActivityPodcastBinding
 import com.nirwashh.android.podplay.repository.ItunesRepo
 import com.nirwashh.android.podplay.service.ItunesService
@@ -16,12 +22,43 @@ class PodcastActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         b = ActivityPodcastBinding.inflate(layoutInflater)
         setContentView(b.root)
+
+
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        return true
+    }
+
+    private fun performSearch(term: String) {
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
-
         GlobalScope.launch {
             val result = itunesRepo.searchByTerm("Android Developer")
             Log.d(TAG, "Results = ${result.body()}")
         }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?:
+            return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
     }
 }
