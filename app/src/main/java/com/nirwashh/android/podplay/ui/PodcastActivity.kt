6f334532670a5e9315912cc66a1421dffaab593e
Nirwash <nirwash@bk.ru>
2022-08-15 21:34:25 +0300

    завершение работы над DetailFragment

--- app/src/main/java/com/nirwashh/android/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/nirwashh/android/podplay/ui/PodcastActivity.kt
@@ -1,12 +1,13 @@
 package com.nirwashh.android.podplay.ui
 
+import android.app.AlertDialog
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
@@ -17,7 +18,9 @@ import com.nirwashh.android.podplay.adapter.PodcastListAdapter
 import com.nirwashh.android.podplay.adapter.PodcastListAdapter.*
 import com.nirwashh.android.podplay.databinding.ActivityPodcastBinding
 import com.nirwashh.android.podplay.repository.ItunesRepo
+import com.nirwashh.android.podplay.repository.PodcastRepo
 import com.nirwashh.android.podplay.service.ItunesService
+import com.nirwashh.android.podplay.viewmodel.PodcastViewModel
 import com.nirwashh.android.podplay.viewmodel.SearchViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -26,9 +29,12 @@ import kotlinx.coroutines.withContext
 
 class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
     private lateinit var b: ActivityPodcastBinding
-    private val searchViewModel by viewModels<SearchViewModel>()
     private lateinit var podcastListAdapter: PodcastListAdapter
+    private lateinit var searchMenuItem: MenuItem
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private val podcastViewModel by viewModels<PodcastViewModel>()
     val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityPodcastBinding.inflate(layoutInflater)
@@ -43,10 +49,11 @@ class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_search, menu)
-        val searchMenuItem = menu?.findItem(R.id.search_item)
-        val searchView = searchMenuItem?.actionView as SearchView
+        searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem.actionView as SearchView
         val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        if (b.podcastRecyclerView.visibility == View.INVISIBLE) searchMenuItem.isVisible = false
         return true
     }
 
@@ -82,6 +89,7 @@ class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
     private fun setupViewModels() {
         val service = ItunesService.instance
         searchViewModel.iTunesRepo = ItunesRepo(service)
+        podcastViewModel.podcastRepo = PodcastRepo()
     }
 
     private fun updateControls() {
@@ -98,7 +106,23 @@ class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
     }
 
     override fun onShowDetails(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData) {
-        //Not implemented yet
+        val feedUrl = podcastSummaryViewData.feedUrl ?: return
+        showProgressBar()
+        val podcast = podcastViewModel.getPodcast(podcastSummaryViewData)
+        hideProgressBar()
+        if (podcast != null) {
+            showDetailsFragment()
+        } else {
+            showError("Error loading feed $feedUrl")
+        }
+    }
+
+    private fun showError(message: String) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok_button), null)
+            .create()
+            .show()
     }
 
     private fun showProgressBar() {
@@ -107,5 +131,30 @@ class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener {
 
     private fun hideProgressBar() {
         b.progressBar.visibility = View.INVISIBLE
+    }
+
+    companion object {
+        private const val TAG_DETAILS_FRAGMENT = "DetailsFragment"
+    }
+
+    private fun createPodcastDetailFragment(): PodcastDetailFragment {
+        var podcastDetailFragment =
+            supportFragmentManager.findFragmentByTag(TAG_DETAILS_FRAGMENT) as
+                    PodcastDetailFragment?
+        if (podcastDetailFragment == null) {
+            podcastDetailFragment = PodcastDetailFragment.newInstance()
+        }
+        return podcastDetailFragment
+    }
+
+    private fun showDetailsFragment() {
+        val podcastDetailFragment = createPodcastDetailFragment()
+        supportFragmentManager.beginTransaction().add(
+            R.id.podcastDetailContainer,
+            podcastDetailFragment,
+            TAG_DETAILS_FRAGMENT
+        ).addToBackStack("DetailsFragment").commit()
+        b.podcastRecyclerView.visibility = View.INVISIBLE
+        searchMenuItem.isVisible = false
     }
 }
--- app/src/main/java/com/nirwashh/android/podplay/ui/PodcastDetailFragment.kt
+++ app/src/main/java/com/nirwashh/android/podplay/ui/PodcastDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.bumptech.glide.Glide
 import com.nirwashh.android.podplay.R
 import com.nirwashh.android.podplay.databinding.FragmentPodcastDetailBinding
 import com.nirwashh.android.podplay.viewmodel.PodcastViewModel
@@ -27,10 +28,26 @@ class PodcastDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        updateControls()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_detail, menu)
+    }
+
+    private fun updateControls() {
+        val viewData = podcastViewModel.activePodcastViewData ?: return
+        dataBinding.feedTitleTextView.text = viewData.feedTitle
+        dataBinding.feedDescTextView.text = viewData.feedDesc
+        activity?.let { activity ->
+            Glide.with(activity).load(viewData.imageUrl).into(dataBinding.feedImageView)
+        }
+    }
+
+    companion object {
+        fun newInstance(): PodcastDetailFragment {
+            return PodcastDetailFragment()
+        }
     }
 }
