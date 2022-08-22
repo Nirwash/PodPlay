90b87d328cd9c3fd0e4e9b04b57987bf6c55ea08
Nirwash <nirwash@bk.ru>
2022-08-22 07:55:15 +0300

    add play and pause features

--- app/src/main/java/com/nirwashh/android/podplay/service/PodplayMediaCallback.kt
+++ app/src/main/java/com/nirwashh/android/podplay/service/PodplayMediaCallback.kt
@@ -1,10 +1,13 @@
 package com.nirwashh.android.podplay.service
 
 import android.content.Context
+import android.media.MediaMetadata
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 
 class PodplayMediaCallback(
     val context: Context,
@@ -15,11 +18,16 @@ class PodplayMediaCallback(
         super.onPlayFromUri(uri, extras)
         println("Playing ${uri.toString()}")
         onPlay()
+        mediaSession.setMetadata(MediaMetadataCompat.Builder()
+            .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI, uri.toString())
+            .build())
+
     }
 
     override fun onPlay() {
         super.onPlay()
         println("onPlay() called")
+        setState(PlaybackStateCompat.STATE_PLAYING)
     }
 
     override fun onStop() {
@@ -30,5 +38,20 @@ class PodplayMediaCallback(
     override fun onPause() {
         super.onPause()
         println("onPause() called")
+        setState(PlaybackStateCompat.STATE_PAUSED)
+    }
+
+    private fun setState(state: Int) {
+        var position: Long = -1
+        val playbackState = PlaybackStateCompat.Builder()
+            .setActions(
+                PlaybackStateCompat.ACTION_PLAY or
+                        PlaybackStateCompat.ACTION_STOP or
+                        PlaybackStateCompat.ACTION_PLAY_PAUSE or
+                        PlaybackStateCompat.ACTION_PAUSE
+            )
+            .setState(state, position, 1.0f)
+            .build()
+        mediaSession.setPlaybackState(playbackState)
     }
 }
