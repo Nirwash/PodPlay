89b0f8340930984975ff5bef68e10ddd3c3010e9
Rubanov Andrey <nirwash@bk.ru>
2022-11-14 07:15:26 +0300

    11

--- app/src/main/java/com/nirwashh/android/podplay/service/PodplayMediaCallback.kt
+++ app/src/main/java/com/nirwashh/android/podplay/service/PodplayMediaCallback.kt
@@ -1,9 +1,12 @@
 package com.nirwashh.android.podplay.service
 
 import android.content.Context
-import android.media.MediaMetadata
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
@@ -14,18 +17,30 @@ class PodplayMediaCallback(
     val mediaSession: MediaSessionCompat,
     var mediaPlayer: MediaPlayer? = null
 ) : MediaSessionCompat.Callback() {
+    private var mediaUrl: Uri? = null
+    private var newMedia: Boolean = false
+    private var mediaExtras: Bundle? = null
+    private var focusRequest: AudioFocusRequest? = null
+
+
     override fun onPlayFromUri(uri: Uri?, extras: Bundle?) {
         super.onPlayFromUri(uri, extras)
         println("Playing ${uri.toString()}")
         onPlay()
-        mediaSession.setMetadata(MediaMetadataCompat.Builder()
-            .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI, uri.toString())
-            .build())
+        mediaSession.setMetadata(
+            MediaMetadataCompat.Builder()
+                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI, uri.toString())
+                .build()
+        )
 
     }
 
     override fun onPlay() {
         super.onPlay()
+        if (ensureAudioFocus()) {
+            mediaSession.isActive = true
+            setState(PlaybackStateCompat.STATE_PLAYING)
+        }
         println("onPlay() called")
         setState(PlaybackStateCompat.STATE_PLAYING)
     }
@@ -41,8 +56,36 @@ class PodplayMediaCallback(
         setState(PlaybackStateCompat.STATE_PAUSED)
     }
 
+    private fun ensureAudioFocus(): Boolean {
+        val audioManager = this.context.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build()
+            val focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(audioAttributes)
+                .build()
+            this.focusRequest = focusRequest
+            val result = audioManager.requestAudioFocus(focusRequest)
+            return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED
+        } else {
+            val result = audioManager.requestAudioFocus(
+                null,
+                AudioManager.STREAM_MUSIC,
+                AudioManager.AUDIOFOCUS_GAIN
+            )
+            return result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED
+        }
+    }
+
+    private fun setNewMedia(uri: Uri?) {
+        newMedia = true
+        mediaUrl = uri
+    }
+
     private fun setState(state: Int) {
-        var position: Long = -1
+        val position: Long = -1
         val playbackState = PlaybackStateCompat.Builder()
             .setActions(
                 PlaybackStateCompat.ACTION_PLAY or
